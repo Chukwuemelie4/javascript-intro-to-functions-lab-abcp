f3fac5a0eea0f7f7276645f34fb45fa0d86e75cf
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-10 19:06:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ function logWhisper(string) {
 }
 logWhisper('HELLO')
 //5 begins
+var yap = "I can\'t hear you!" 
+var yess =  "YES INDEED!"
+var got = "I love you, too."
+function sayHiToGrandma(string) {
+  if (string==string.toLowerCase()){
+    return yap }
+    else if (string==string.toUpperCase()) {
+      return yess }
+      else {
+        return got }
+}
+console.log(sayHiToGrandma('hello'))
+console.log(sayHiToGrandma('HELLO'))
+console.log(sayHiToGrandma("I love you, Grandma."))
