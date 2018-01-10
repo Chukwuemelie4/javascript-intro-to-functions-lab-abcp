1e5606cb0801b01f6a6c184d1372af6de051c131
Chukwuemelie Oguchi <chukwuemelie4@gmail.com>
2018-01-10 19:06:37 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+// 1 begins
+function shout(string){
+  return string.toUpperCase()
+}
+shout('hello')
+//2 begins
+function whisper(string){
+  return string.toLowerCase()
+}
+whisper('HELLO')
+// 3 begins
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+logShout('hello')
+//4 begins
+function logWhisper(string) {
+  console.log('string'.toLowerCase())
+}
+logWhisper('HELLO')
+//5 begins
